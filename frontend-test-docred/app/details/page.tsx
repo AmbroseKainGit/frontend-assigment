fbf5ae0f39c32edb21196168c85b7d7a6ade0e5e
Cristian Ceballos <cmceballos88@gmail.com>
2023-10-29 19:58:48 +0100

    Delete unused files normalize css create folder structure and update apollo provider

---
+++ frontend-test-docred/app/details/page.tsx
@@ -0,0 +1,9 @@
+export default function Details() {
+    return (
+      <div style={{color: 'red'}}>
+        <h1>Cards</h1>
+        <p>asdasdas</p>
+      </div>
+    );
+  }
+  
--- frontend-test-docred/lib/Graphql/Provider.tsx
+++ frontend-test-docred/lib/Graphql/Provider.tsx
@@ -1,8 +1,17 @@
-import { ApolloClient, InMemoryCache } from '@apollo/client';
+"use client";
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 
 const client = new ApolloClient({
   uri: 'https://rickandmortyapi.com/graphql',
   cache: new InMemoryCache(),
 });
 
-export default client;
+interface IGraphQlProviderProps {
+  children: React.ReactNode;
+}
+
+const GraphQlProvider: React.FC<IGraphQlProviderProps> = ({ children }) => {
+  return <ApolloProvider client={client}>{children}</ApolloProvider>;
+};
+
+export default GraphQlProvider;
